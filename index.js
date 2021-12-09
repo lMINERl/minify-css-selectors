8fb184149f0b5de8dd5e5a81e76b9660a47185e7
lMINERl <>
2021-12-09 20:49:39 +0200

    a test 1.0.0.0.0.0.0.0.0000..00

--- index.js
+++ index.js
@@ -68,65 +68,38 @@ module.exports = postcss.plugin("minify-css-selectors", (options = {
 
         const dirPath = options.jsDirectoryPath;
 
-        const files = fs.readdirSync(dirPath)
-        files.filter(v => (/.+\.js/gm).test(v)).forEach(file => {
-            let rf = fs.readFileSync(`${dirPath}\\${file}`, { encoding: 'utf8' });
+        fs.readdir(dirPath, { encoding: 'utf8' }, (dirErr, files) => {
+            if (dirErr) {
+                console.error(dirErr.message);
+                return;
+            }
 
-            const classesToReplace = selectors['.'];
-            const funcGetClassName = rf.match(/get\ ?[Cc]lass[Nn]ame\(\)\{.*\}/gm);
-            const inlineClassName = rf.match(/(class|className)\=\"[A-Za-z0-9\_\-\ ]+\"/gm);
-            const objClassName = rf.match(/(class|className)\:\"[A-Za-z0-9\_\-\ ]+\"/gm);
-            const objClassNameCondition = rf.match(/(class|className)\:[a-zA-Z0-9\.]+\?(\"[a-zA-Z0-9\_\-\ \0]*?\")\:(\"[a-zA-Z0-9\_\-\ \0]*?\")/gm);
+            files.filter(v => (/.+\.js/gm).test(v)).forEach(file => {
+                fs.readFile(`${dirPath}\\${file}`, { encoding: 'utf8' }, (fileErr, data) => {
+                    if (fileErr) {
+                        console.error(fileErr.message);
+                        return;
+                    }
+                    let rf = data;
+                    Object.keys(selectors['.']).forEach(key => {
+                        rf = rf.replaceAll(new RegExp(`[\\'\\"]\\b${key}\\b[\\"\\']`, 'gm'), `"${selectors['.'][key]}"`);
+                    })
+                    Object.keys(properties).forEach(key => {
+                        rf = rf.replaceAll(new RegExp(`var\\(${key}\\)`, 'gm'), `var(${properties[key]})`)
+                    })
 
-            let originalClasses = Object.entries(classesToReplace);
-            let orignKeys = originalClasses.map(k => k[0]);
-            let orignValues = originalClasses.map(k => k[1]);
+                    fs.writeFile(`${options.outPutPath}/${file}`, rf, () => {
+                        console.log(`${file}:finished`);
+                    });
 
-            let orIndex = -1;
-            let exist = false;
-            funcGetClassName?.forEach(v => {
-                orIndex = -1;
-                exist = orignKeys.some((or, i) => {
-                    orIndex = i;
-                    return v.includes(or);
-                });
-                if (exist) {
-                    rf = rf.replace(v, v.replace(new RegExp(`\\b${orignKeys[orIndex]}\\b`), orignValues[orIndex]));
-                }
-            });
-            inlineClassName?.forEach(v => {
-                orIndex = -1;
-                exist = orignKeys.some((or, i) => {
-                    orIndex = i;
-                    return v.split('class=').filter(val => val != '').map(val => val.replaceAll(`"`, ``)).map(val => val.split(' ')).flat().includes(or);
-                });
-                if (exist) {
-                    rf = rf.replace(v, v.replace(new RegExp(`\\b${orignKeys[orIndex]}\\b`), orignValues[orIndex]));
-                }
+                })
+
+
+
             });
 
-            objClassName?.forEach(v => {
-                orIndex = -1;
-                exist = orignKeys.some((or, i) => {
-                    orIndex = i;
-                    return v.split(`className:`).filter(val => val != '').map(val => val.replaceAll(`"`, ``)).map(val => val.split(' ')).flat().includes(or);
-                });
-                if (exist) {
-                    rf = rf.replace(v, v.replace(new RegExp(`\\b${orignKeys[orIndex]}\\b`), orignValues[orIndex]));
-                }
-            });
-            objClassNameCondition?.forEach(v => {
-                orIndex = -1;
-                exist = orignKeys.some((or, i) => {
-                    orIndex = i;
-                    return v.split('?').filter((val, i) => i != 0).map(val => val.split(':').map(c => c.replaceAll('"', ''))).flat().includes(or);
-                });
-                if (exist) {
-                    rf = rf.replace(v, v.replace(new RegExp(`\\b${orignKeys[orIndex]}\\b`), orignValues[orIndex]));
-                }
-            });
-
-            fs.writeFileSync(`${options.outPutPath}/${file}`, rf);
         });
+
+
     };
 });
